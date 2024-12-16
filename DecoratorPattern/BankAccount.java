9edb88a134edfbd24d44e3d1b862c2e0ab172662
Jairus Joshua Ramos <ramosjairus6776@gmail.com>
2024-12-16 22:08:16 +0800

    Create BankAccount.java

---
+++ DecoratorPattern/BankAccount.java
@@ -0,0 +1,13 @@
+package DecoratorPattern;
+
+public interface BankAccount {
+
+    String showAccountType();
+    Double getInterestRate();
+    Double getBalance();
+    String showBenefits();
+    Double computeBalanceWithInterest();
+    String showInfo();
+
+
+}
